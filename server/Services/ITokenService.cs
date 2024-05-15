1d43a5b17d33fc6b7f70e6f834b3cafc68493655
Sora <sora.pedrob@gmail.com>
2024-05-15 16:37:17 -0300

    Token interface

---
+++ server/Services/ITokenService.cs
@@ -0,0 +1,14 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace server.Services
+{
+    public interface ITokenService
+    {
+        JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config);
+    }
+}
